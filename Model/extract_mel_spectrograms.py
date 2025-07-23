a63fdb0b9248ded0dff3cd77cd9d2e53fab1a728
JosephJantke <josephjantke365@outlook.com.au>
2025-07-23 16:45:42 +0930

    20250723

--- Model/extract_mel_spectrograms.py
+++ Model/extract_mel_spectrograms.py
@@ -1,16 +1,19 @@
 import torch
 import torchaudio
 
+### convert wav files into melspectrograms
 test_wav = "C:/Josephs_PhD/Recogniser/test_set/TEST/1_prob_064_time_00110.3.wav"
 
 waveform, sample_rate = torchaudio.load(test_wav, normalize=True)
+
+#replicate birdnet melspectrogram parameters
 transform = torchaudio.transforms.MelSpectrogram(
-    sample_rate,
-    n_fft = 1024,
-    n_mels = 64,
+    sample_rate = 32000,
+    n_fft = 2048,
+    n_mels = 48,
     hop_length=512,
-    f_min = 1000,
-    f_max = 5000,
+    f_min = 0,
+    f_max = 12000,
     power = 2.0
     )
 mel_specgram = transform(waveform)  # (channel, n_mels, time)
--- Model/model.py
+++ Model/model.py
@@ -1,35 +1,4 @@
 import torch
 import torch.nn as nn
 
-class SeedModel(nn.Module):
-    def __init__(self, cnn_backbone, transformer_dim=256, num_heads=4, num_layers=2):
-        super().__init__()
-        self.cnn = cnn_backbone  # BirdNET or custom CNN
-        for param in self.cnn.parameters():
-            param.requires_grad = False  # Freeze CNN at seed stage
-
-        self.projector = nn.Linear(cnn_output_dim, transformer_dim)
-
-        encoder_layer = nn.TransformerEncoderLayer(
-            d_model=transformer_dim,
-            nhead=num_heads,
-            dim_feedforward=512,
-            batch_first=True
-        )
-        self.transformer = nn.TransformerEncoder(encoder_layer, num_layers=num_layers)
-        self.classifier = nn.Sequential(
-            nn.Linear(transformer_dim, 128),
-            nn.ReLU(),
-            nn.Linear(128, 1),
-            nn.Sigmoid()
-        )
-
-    def forward(self, x):
-        x = self.cnn(x)  # shape: [B, C, H, W]
-        x = x.mean(dim=2)  # average over frequency → shape: [B, C, T]
-        x = x.permute(0, 2, 1)  # [B, T, C] for Transformer
-        x = self.projector(x)  # project to transformer dim
-        x = self.transformer(x)  # [B, T, D]
-        x = x.mean(dim=1)  # pool across time
-        return self.classifier(x).squeeze(1)  # [B]
-
+import tensorflow as tf
